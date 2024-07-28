fcc3780084c30e1c48370ae9c07e18be4a7732d0
Taylor Reis <reist98822@gmail.com>
2024-07-28 09:50:12 -0300

    Chat Bot

---
+++ Jarvis-AI-main/app.js
@@ -0,0 +1,118 @@
+const btn = document.querySelector(".talk");
+const content = document.querySelector(".content");
+
+function speak(text) {
+  const text_speak = new SpeechSynthesisUtterance(text);
+  text_speak.rate = 1;
+  text_speak.volume = 1;
+  text_speak.pitch = 1;
+  window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+  const day = new Date();
+  const hour = day.getHours();
+
+  if (hour >= 0 && hour < 12) {
+    speak("Bom dia Chefe...");
+  } else if (hour >= 12 && hour < 17) {
+    speak("Boa tarde Master...");
+  } else {
+    speak("Boa Noite Sir...");
+  }
+}
+
+window.addEventListener("load", () => {
+  speak("Iniciando JARVIS...");
+  wishMe();
+});
+
+// Verifica se o navegador suporta a API
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if (SpeechRecognition) {
+  const recognition = new SpeechRecognition();
+
+  recognition.onstart = function () {
+    console.log("Reconhecimento de fala iniciado");
+  };
+
+  recognition.onspeechend = function () {
+    console.log("Reconhecimento de fala terminado");
+    recognition.stop();
+  };
+
+  recognition.onresult = function (event) {
+    const currentIndex = event.resultIndex;
+    const transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(transcript.toLowerCase());
+  };
+
+  btn.addEventListener("click", () => {
+    content.textContent = "Listening...";
+    recognition.start();
+  });
+} else {
+  console.log("Seu navegador não suporta a API de reconhecimento de fala");
+}
+
+function takeCommand(message) {
+  if (message.includes("ola") || message.includes("ei, jarvis")) {
+    speak("Ola Senhor(a), Como eu posso ajudar você?");
+  } else if (message.includes("Abrir google")) {
+    window.open("https://google.com", "_blank");
+    speak("Opening Google...");
+  } else if (message.includes("Abrir youtube")) {
+    window.open("https://youtube.com", "_blank");
+    speak("Opening Youtube...");
+  } else if (message.includes("Abrir facebook")) {
+    window.open("https://facebook.com", "_blank");
+    speak("Opening Facebook...");
+  } else if (
+    message.includes("what is") ||
+    message.includes("who is") ||
+    message.includes("what are")
+  ) {
+    window.open(
+      `https://www.google.com/search?q=${message.replace(" ", "+")}`,
+      "_blank"
+    );
+    const finalText =
+      "This is what I found on the internet regarding " + message;
+    speak(finalText);
+  } else if (message.includes("wikipedia")) {
+    window.open(
+      `https://en.wikipedia.org/wiki/${message.replace("wikipedia", "").trim()}`,
+      "_blank"
+    );
+    const finalText = "This is what I found on Wikipedia regarding " + message;
+    speak(finalText);
+  } else if (message.includes("time")) {
+    const time = new Date().toLocaleString(undefined, {
+      hour: "numeric",
+      minute: "numeric",
+    });
+    const finalText = "The current time is " + time;
+    speak(finalText);
+  } else if (message.includes("date")) {
+    const date = new Date().toLocaleString(undefined, {
+      month: "short",
+      day: "numeric",
+    });
+    const finalText = "Today's date is " + date;
+    speak(finalText);
+  } else if (message.includes("calculator")) {
+    window.open("Calculator:///");
+    const finalText = "Opening Calculator";
+    speak(finalText);
+  } else {
+    window.open(
+      `https://www.google.com/search?q=${message.replace(" ", "+")}`,
+      "_blank"
+    );
+    const finalText = "I found some information for " + message + " on Google";
+    speak(finalText);
+  }
+}
